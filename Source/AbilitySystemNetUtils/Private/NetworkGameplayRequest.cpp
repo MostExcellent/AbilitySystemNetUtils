9ebeb5ce7608c9f19efd3d299394e7fa02a0ff4c
David Glover <MostExcellent@users.noreply.github.com>
2025-11-11 18:23:41 -0800

    FNetworkGameplayRequestInfo to contain timestamp, RTT estimate, reconciliation ID, and bitmask for which ones are set

---
+++ Source/AbilitySystemNetUtils/Private/NetworkGameplayRequest.cpp
@@ -0,0 +1,64 @@
+﻿/*
+MIT License
+
+Copyright (c) 2025 David Glover
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include "NetworkGameplayRequest.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(NetworkGameplayRequest)
+
+void FNetworkGameplayRequestInfo::SetTimestamp(const float InTime)
+{
+	Timestamp = InTime;
+	Flags |= static_cast<uint8>(ENetRequestFlags::Timestamp);
+}
+
+void FNetworkGameplayRequestInfo::SetEstimateRTT(const float InRTT)
+{
+	EstimateRTT = InRTT;
+	Flags |= static_cast<uint8>(ENetRequestFlags::EstimateRTT);
+}
+
+void FNetworkGameplayRequestInfo::SetReconciliationId(const int32 InId)
+{
+	ReconciliationId = InId;
+	Flags |= static_cast<uint8>(ENetRequestFlags::ReconciliationId);
+}
+
+bool FNetworkGameplayRequestInfo::NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess)
+{
+	if (Flags & static_cast<int32>(ENetRequestFlags::Timestamp))
+	{
+		Ar << Timestamp;
+	}
+	if (Flags & static_cast<int32>(ENetRequestFlags::EstimateRTT))
+	{
+		Ar << EstimateRTT;
+	}
+	if (Flags & static_cast<int32>(ENetRequestFlags::ReconciliationId))
+	{
+		Ar << ReconciliationId;
+	}
+	
+	bOutSuccess = true;
+	return true;
+}
---
+++ Source/AbilitySystemNetUtils/Public/NetworkGameplayRequest.h
@@ -0,0 +1,94 @@
+﻿/*
+MIT License
+
+Copyright (c) 2025 David Glover
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NetworkGameplayRequest.generated.h"
+
+UENUM(BlueprintType, meta = (Bitflags, UseEnumValuesAsMaskValuesInEditor = "true"))
+enum class ENetRequestFlags : uint8
+{
+	None = 0 UMETA(Hidden),
+	Timestamp = 1 << 0,
+	EstimateRTT = 1 << 1,
+	ReconciliationId = 1 << 2,
+};
+
+/**
+ * Packages the core request timing/replication info.
+ */
+USTRUCT(BlueprintType)
+struct FNetworkGameplayRequestInfo
+{
+	GENERATED_BODY()
+	
+	FNetworkGameplayRequestInfo() : Timestamp(0.f), EstimateRTT(0.f), ReconciliationId(0), Flags(0)
+	{
+	}
+	
+	FNetworkGameplayRequestInfo(const float InTime, const float InRTT, const int32 InId) : Flags(0)
+	{
+		Timestamp = InTime;
+		EstimateRTT = InRTT;
+		ReconciliationId = InId;
+		Flags |= static_cast<uint8>(ENetRequestFlags::Timestamp);
+		Flags |= static_cast<uint8>(ENetRequestFlags::EstimateRTT);
+		Flags |= static_cast<uint8>(ENetRequestFlags::ReconciliationId);
+	}
+
+	UPROPERTY(BlueprintReadWrite, Category = "Targeting|Replication")
+	float Timestamp;
+	
+	UPROPERTY(BlueprintReadWrite, Category = "Targeting|Replication")
+	float EstimateRTT;
+	
+	UPROPERTY(BlueprintReadWrite, Category = "Targeting|Replication")
+	float ReconciliationId;
+	
+	UPROPERTY(BlueprintReadWrite, Category = "Targeting|Replication", meta = (Bitmask, BitmaskEnum = "ENetRequestFlags", ExposeOnSpawn = true))
+	int32 Flags;
+	
+	bool CheckFlag(ENetRequestFlags InFlag) const
+	{
+		return Flags & static_cast<int32>(InFlag);
+	}
+	
+	void SetTimestamp(const float InTime);
+	
+	void SetEstimateRTT(const float InRTT);
+	
+	void SetReconciliationId(const int32 InId);
+	
+	bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess);
+};
+
+template <>
+struct TStructOpsTypeTraits<FNetworkGameplayRequestInfo> : public TStructOpsTypeTraitsBase2<FNetworkGameplayRequestInfo>
+{
+	enum
+	{
+		WithNetSerializer = true
+	};
+};
